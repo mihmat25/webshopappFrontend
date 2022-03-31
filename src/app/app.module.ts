2cf64066ff57ef400a62d8f5bfadf5b6520ec014
MihalyMatyas <mihamatyas@gmail.com>
2022-03-31 17:36:25 +0300

    buttons aligned in the toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppComponent } from './app.component';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from "@angular/common/http";
-import { ProductComponent } from './product/product.component';
 // import { OrderComponent } from './order/order.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { ProductComponent } from './product/product.component';
 
 
 
